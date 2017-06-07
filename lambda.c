f977bcdb4d0fe74df37545c72a09daf77ec30a04
Gregoire Lodi <gregoire.lodi@epitech.eu>
2017-06-07 13:31:01 +0200

    Better lambda exampel

--- lambda.c
+++ lambda.c
@@ -8,11 +8,9 @@ int main(int ac, char **av)
   if (ac != 3)
     return !!printf("Usage: %s x y\n", av[0]);
 
-  char *s = calloc(10, sizeof(char));
-
-  s = lambda(char *, (int x, int y) {
+  int x = atoi(av[1]);
+  int y = atoi(av[2]);
+  printf("%i > %i: %s", x, y, lambda(char *, (int x, int y) {
     return x > y ? "true" : "false";
-  })(atoi(av[1]), atoi(av[2]));
-
-  printf("%i > %i: %s\n", atoi(av[1]), atoi(av[2]), s);
+  })(x, y));
 }
